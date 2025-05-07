944494ba3a80b0e9ca0caacae98d51ad5830e83e
Lea Meubrink <lmeubrin@student.42berlin.de>
2025-05-07 11:02:42 +0200

    it's leaking

--- tests/get_next_line_test.c
+++ tests/get_next_line_test.c
@@ -6,7 +6,7 @@
 /*   By: lmeubrin <lmeubrin@student.42berlin.d      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 16:56:01 by lmeubrin          #+#    #+#             */
-/*   Updated: 2025/05/06 18:18:26 by lmeubrin         ###   ########.fr       */
+/*   Updated: 2025/05/07 10:57:06 by lmeubrin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,16 @@ int	main(void)
 	char	*line;
 	int		fd;
 	int		fd2;
+	int		fd3;
 	int		i;
 	int		j = 1;
 
 	fd = open("bumps.rt", O_RDONLY);
 	fd2 = open("testfile2", O_RDONLY);
+	fd3 = open("testfile_no_nl", O_RDONLY);
 	if (fd == -1)
 		return (EXIT_FAILURE);
-	if (fd2 == -1)
+	if (fd2 == -1 || fd3 == -1)
 		return (EXIT_FAILURE);
 	line = get_next_line(fd);
 	i = 1;
@@ -39,6 +41,9 @@ int	main(void)
 		printf("%d:'%s'\n", j, line);
 		j++;
 		i++;
+		line = get_next_line(fd3);
+		printf("third file:\n");
+		printf("%d:'%s'\n", j, line);
 		line = get_next_line(fd);
 	}
 	return (EXIT_SUCCESS);
